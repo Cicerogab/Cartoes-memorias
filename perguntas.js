6c60fb00dbc872ac61889f8f410d724f8346010c
Cicerogab <161970787+Cicerogab@users.noreply.github.com>
2024-11-03 00:08:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Gabriel?',
+'Gabriel: Significa “homem de Deus”, “homem forte de Deus”, “fortaleza de Deus”, “mensageiro de Deus”.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome  Edivânia?',
+    'Edivânia: Significa “amiga valiosa”, “amiga amada e querida”. Edivânia é um nome brasileiro cuja origem é germânica. Surge do nome inglês Edwin a partir do germânio Eadwine (o mesmo que Eduíno ou Eduína, em português).'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Samuel?',
+    'Samuel: Significa “nome de Deus” ou "Deus ouve". Com popularidade em diferentes países, como Nigéria, Gana, Estados Unidos e Brasil, Samuel é um nome forte e bonito com origens associadas à Bíblia.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Thiago?',
+    'Thiago: Significa “aquele que vem do calcanhar”. O nome Thiago é uma variante de Tiago. Surgiu como uma variação de Iago, através de Santiago, nome vindo do espanhol que tem origem na aglutinação Sant’Iago.'
+)
